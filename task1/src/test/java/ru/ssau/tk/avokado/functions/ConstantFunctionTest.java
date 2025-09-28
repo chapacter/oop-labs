7ad3c270e8a0c02dfdb31398e20c035809c86d99
Беляков <bila09548@gmail.com>
2025-09-29 01:44:47 +0400

    Добавил ConstantFunction и тесты для него

---
+++ task1/src/test/java/ru/ssau/tk/avokado/functions/ConstantFunctionTest.java
@@ -0,0 +1,30 @@
+package ru.ssau.tk.avokado.functions;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConstantFunctionTest {
+
+    @Test
+    void testConstantFunction() {
+        // Прописываем различные константы для дальнейшего тестирования
+        ConstantFunction func1 = new ConstantFunction(75.0);
+        ConstantFunction func2 = new ConstantFunction(-451.1941);
+        ConstantFunction func3 = new ConstantFunction(0.0);
+        ConstantFunction func4 = new ConstantFunction(893.128);
+
+        assertEquals(75.0, func1.apply(107.1), 1e-9);
+        assertEquals(75.0, func1.apply(0.0), 1e-9);
+        assertEquals(75.0, func1.apply(-50.43), 1e-9);
+
+        assertEquals(-451.1941, func2.apply(10), 1e-9);
+        assertEquals(0.0, func3.apply(999), 1e-9);
+        assertEquals(893.128, func4.apply(9099), 1e-9);
+    }
+
+    @Test
+    void testGetConstantMethod() {
+        ConstantFunction func = new ConstantFunction(894.3);
+        assertEquals(894.3, func.getConstant(), 1e-9);
+    }
+}
