39d9945c5f664853c5662f136581d0eb3a34809c
Chapacter <artem6453a@gmail.com>
2025-09-29 21:55:00 +0400

    Импортировано из task2

--- task3/src/main/java/ru/ssau/tk/avokado/lab2/functions/UnitFunction.java
+++
@@ -1,8 +0,0 @@
-package ru.ssau.tk.avokado.functions;
-
-public class UnitFunction extends ConstantFunction {
-    // Прописываем конструктор для UnitFunction
-    public UnitFunction() {
-        super(1.0); // Вызываем таким образом конструктор родителя с аргументом 0.0
-    }
-}
