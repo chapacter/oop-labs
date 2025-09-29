e5d61a0e4dd858bea7fb7cc93f7c88c0eeb8431b
Chapacter <artem6453a@gmail.com>
2025-09-29 18:17:53 +0400

    Дублирование из task1

---
+++ task2/src/main/java/ru/ssau/tk/avokado/lab2/functions/UnitFunction.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.avokado.functions;
+
+public class UnitFunction extends ConstantFunction {
+    // Прописываем конструктор для UnitFunction
+    public UnitFunction() {
+        super(1.0); // Вызываем таким образом конструктор родителя с аргументом 0.0
+    }
+}
---
+++ task2/src/main/java/ru/ssau/tk/avokado/lab2/functions/ZeroFunction.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.avokado.lab2.functions;
+
+public class ZeroFunction extends ConstantFunction {
+    // Прописываем конструктор для ZeroFunction
+    public ZeroFunction() {
+        super(0.0); // Вызываем таким образом конструктор родителя с аргументом 0.0
+    }
+}
