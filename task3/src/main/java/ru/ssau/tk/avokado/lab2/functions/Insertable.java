f580ba4ef6d41895d978ca6191edaf8c035d6276
Chapacter <126392664+chapacter@users.noreply.github.com>
2025-10-03 23:54:01 +0400

    Delete task3/src directory

--- task3/src/main/java/ru/ssau/tk/avokado/lab2/functions/Insertable.java
+++
@@ -1,5 +0,0 @@
-package ru.ssau.tk.avokado.lab2.functions;
-
-public interface Insertable {
-    void insert(double x, double y);
-}
--- task3/src/main/java/ru/ssau/tk/avokado/lab2/functions/Removable.java
+++
@@ -1,5 +0,0 @@
-package ru.ssau.tk.avokado.lab2.functions;
-
-public interface Removable {
-    void remove(int index);
-}
