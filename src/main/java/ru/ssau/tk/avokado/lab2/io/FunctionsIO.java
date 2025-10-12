d4122d5c78226d7ea14299510c35732145fdebdb
Chapacter <artem6453a@gmail.com>
2025-10-12 15:02:26 +0400

    lab3.3: добавлен TabulatedFunctionFileOutputStream

--- src/main/java/ru/ssau/tk/avokado/lab2/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/avokado/lab2/io/FunctionsIO.java
@@ -1,8 +1,29 @@
 package ru.ssau.tk.avokado.lab2.io;
-public final class FunctionsIO {
 
+import ru.ssau.tk.avokado.lab2.functions.Point;
+import ru.ssau.tk.avokado.lab2.functions.TabulatedFunction;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException("Utility class");
     }
 
+//    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+//        // Для Y
+//    }
+
+    static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        dataOutputStream.writeInt(function.getCount());
+
+        for (Point point : function) {
+            dataOutputStream.writeDouble(point.x);
+            dataOutputStream.writeDouble(point.y);
+        }
+        dataOutputStream.flush();
+    }
 }
---
+++ src/main/java/ru/ssau/tk/avokado/lab2/io/TabulatedFunctionFileOutputStream.java
@@ -0,0 +1,34 @@
+package ru.ssau.tk.avokado.lab2.io;
+
+import ru.ssau.tk.avokado.lab2.functions.ArrayTabulatedFunction;
+import ru.ssau.tk.avokado.lab2.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.avokado.lab2.functions.TabulatedFunction;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class TabulatedFunctionFileOutputStream {
+    public static void main(String[] args) {
+        double[] xValues = {1.0, 2.0, 3.0, 4.0, 5.0};
+        double[] yValues = {1.5, 32.4, 56.2, 23.0, 1.0};
+
+        TabulatedFunction arrayFunction = new ArrayTabulatedFunction(xValues, yValues);
+        TabulatedFunction linkedListFunction = new LinkedListTabulatedFunction(xValues, yValues);
+
+        try (BufferedOutputStream arrayStream = new BufferedOutputStream( new FileOutputStream("output/array function.bin"));
+                BufferedOutputStream linkedListStream = new BufferedOutputStream( new FileOutputStream("output/linked list function.bin"))
+        ) {
+            FunctionsIO.writeTabulatedFunction(arrayStream, arrayFunction);
+            FunctionsIO.writeTabulatedFunction(linkedListStream, linkedListFunction);
+
+            System.out.println("Функции успешно записаны в файлы:");
+            System.out.println("- output/array function.bin");
+            System.out.println("- output/linked list function.bin");
+        }
+        catch (IOException e) {
+            System.err.println("Ошибка при записи файлов:");
+            e.printStackTrace();
+        }
+    }
+}
