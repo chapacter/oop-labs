3902514c4726ce34e0d0145fc11de99fc144d300
Chapacter <artem6453a@gmail.com>
2025-10-13 21:41:09 +0400

    lab4.1: Добавление ReadTask

--- src/main/java/ru/ssau/tk/avokado/lab2/concurrent/ReadTask.java
+++ src/main/java/ru/ssau/tk/avokado/lab2/concurrent/ReadTask.java
@@ -1,4 +1,25 @@
 package ru.ssau.tk.avokado.lab2.concurrent;
 
-public class ReadTask {
+import ru.ssau.tk.avokado.lab2.functions.TabulatedFunction;
+
+public class ReadTask implements Runnable {
+    private final TabulatedFunction function;
+
+    public ReadTask(TabulatedFunction function) {
+        if (function == null) {
+            throw new IllegalArgumentException("Function cannot be null");
+        }
+        this.function = function;
+    }
+
+    @Override
+    public void run() {
+        int count = function.getCount();
+        for (int i = 0; i < count; i++) {
+            double x = function.getX(i);
+            double y = function.getY(i);
+            System.out.printf("After read: i = %d, x = %f, y = %f%n", i, x, y);
+        }
+
+    }
 }
