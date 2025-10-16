27bf6c07ff69c5b6655b60881a4d4e4770e849e3
Chapacter <artem6453a@gmail.com>
2025-10-16 22:47:58 +0400

    lab4.1: Добавление ReadWriteTaskExecutor

--- src/main/java/ru/ssau/tk/avokado/lab2/concurrent/ReadWriteTaskExecutor.java
+++ src/main/java/ru/ssau/tk/avokado/lab2/concurrent/ReadWriteTaskExecutor.java
@@ -1,4 +1,15 @@
 package ru.ssau.tk.avokado.lab2.concurrent;
 
+import ru.ssau.tk.avokado.lab2.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.avokado.lab2.functions.TabulatedFunction;
+import ru.ssau.tk.avokado.lab2.functions.ConstantFunction;
+
 public class ReadWriteTaskExecutor {
+    public static void main(String[] args) {
+        TabulatedFunction function = new LinkedListTabulatedFunction(new ConstantFunction(-1), 1, 1000, 10000);
+        ReadTask rTask = new ReadTask(function);
+        WriteTask wTask = new WriteTask(function, 0.5);
+        new Thread(rTask).start();
+        new Thread(wTask).start();
+    }
 }
