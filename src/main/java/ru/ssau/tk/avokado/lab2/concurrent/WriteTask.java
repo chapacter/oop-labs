a221c3c25a2a65844b965e9e1d6b7439a8d5bb1e
Chapacter <artem6453a@gmail.com>
2025-10-16 19:38:51 +0400

    lab4.1: Добавление WriteTask

--- src/main/java/ru/ssau/tk/avokado/lab2/concurrent/WriteTask.java
+++ src/main/java/ru/ssau/tk/avokado/lab2/concurrent/WriteTask.java
@@ -1,4 +1,24 @@
 package ru.ssau.tk.avokado.lab2.concurrent;
 
-public class WriteTask {
+import ru.ssau.tk.avokado.lab2.functions.TabulatedFunction;
+
+public class WriteTask implements Runnable {
+    private final TabulatedFunction function;
+    private final double value;
+
+    public WriteTask(TabulatedFunction function, double value) {
+        if (function == null) { throw new IllegalArgumentException("Function == null");}
+//        if (value == null) { throw new IllegalArgumentException("Value == null");}
+
+        this.function = function;
+        this.value = value;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < function.getCount(); i++) {
+            function.setY(i, value);
+            System.out.printf("Writing for index %d complete%n", i);
+        }
+    }
 }
