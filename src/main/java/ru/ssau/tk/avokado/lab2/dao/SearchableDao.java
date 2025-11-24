42486582f1ddae2b889875a51d1492b5410c007b
Chapacter <artem6453a@gmail.com>
2025-11-24 21:05:14 +0400

    lab5: dao

---
+++ src/main/java/ru/ssau/tk/avokado/lab2/dao/SearchableDao.java
@@ -0,0 +1,22 @@
+package ru.ssau.tk.avokado.lab2.dao;
+
+import ru.ssau.tk.avokado.lab2.dto.SearchCriteria;
+import ru.ssau.tk.avokado.lab2.dto.SearchResult;
+import java.util.List;
+
+public interface SearchableDao<T> {
+
+    SearchResult<T> search(SearchCriteria criteria);
+
+    List<T> findAll();
+
+    List<T> findByField(String fieldName, Object value);
+
+    List<T> findByFieldLike(String fieldName, String pattern);
+
+    List<T> findByFieldIn(String fieldName, List<?> values);
+
+    List<T> findWithPagination(int page, int pageSize);
+
+    List<T> findWithSorting(String sortField, SearchCriteria.SortDirection direction);
+}
